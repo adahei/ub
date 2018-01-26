9bc2c56fc8dfc607cac8f033b5eb4040bfdbbf88
Adam Heidmark <adam@heidmark.se>
2018-01-26 13:32:25 +0100

    Add favicons and other touch icons

---
+++ src/components/UB/Share.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="social-buttons">
+    <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A//utvecklarbolaget.se/job" class="icon-social icon-facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+    <a href="https://twitter.com/home?status=%40Utv_bolaget" class="icon-social icon-facebook" target="_blank"><i class="fa fa-twitter"></i></a>
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url=http%3A//utvecklarbolaget.se/job&title=Utvecklarbolaget&summary=Tillsammans%20bygger%20vi%20framtiden%20-%20i%20kod,%20i%20m%C3%A4nniska,%20i%20bolag&source=" class="icon-social icon-linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.social-buttons {
+  margin-top: 0;
+  .icon-social {
+    display: inline-block;
+    font-size: 2rem;
+    padding: 0 1rem;
+  }
+}
+</style>
